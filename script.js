f84dd6321b0acd17e89b29183df1a77bda3bacd0
Dziyana Arabei <darabei@ibagroup.eu>
2019-06-10 15:01:39 +0300

    add operators

--- script.js
+++ script.js
@@ -5,4 +5,21 @@ let person ={
     age: 25
 }
 
-console.log(person["name"], person.name);
+// console.log(person["name"], person.name);
+// var years = prompt('Сколько вам лет?', 100);
+
+// alert('Вам ' + years + ' лет!')
+
+// var test = prompt("Тест", '');
+
+// var age = prompt('возраст?', 18);
+
+// var message = (age < 3) ? 'Здравствуй, малыш!' :
+//   (age < 18) ? 'Привет!' :
+//   (age < 100) ? 'Здравствуйте!' :
+//   'Какой необычный возраст!';
+
+// alert( message );
+console.log(typeof(null));  //object
+console.log(typeof(45 +"456"));  //string
+console.log(typeof(45 + +"456"));  //number
